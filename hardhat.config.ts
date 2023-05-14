9ff3e51ee4dd92acea479fe16c5f194ece298d85
Rahul Guha <69rahul16@gmail.com>
2023-05-14 11:41:27 +0530

    adding contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+require("dotenv").config();
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.Private_key],
+    },
+  },
 };
 
 export default config;
